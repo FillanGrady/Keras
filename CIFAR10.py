253256bda7291bbbb32170b988fd7b01b8497e3f
Fillan Grady <fillangrady@gmail.com>
2018-04-11 22:36:48 -0500

    Changed CIFAR to take advantage of data augmentation

--- CIFAR10.py
+++ CIFAR10.py
@@ -1,4 +1,6 @@
 import keras
+import keras.preprocessing.image
+import time
 
 
 def compare(image, predict, correct):
@@ -27,11 +29,11 @@ def get_data():
 
 
 if __name__ == "__main__":
+    data_augmentation = True
+    epochs = 50
+    batch_size = 32
     X_train, Y_train, X_test, Y_test = get_data()
-    print("X_train shape: %s" % str(X_train.shape))
-    print("Y_train shape: %s" % str(Y_train.shape))
-    print("X_test shape: %s" % str(X_test.shape))
-    print("Y_test shape: %s" % str(Y_test.shape))
+    start = time.time()
     model = keras.models.Sequential()
     model.add(keras.layers.Convolution2D(filters=32, kernel_size=3, padding='same', activation='relu', input_shape=(32, 32, 3)))
     model.add(keras.layers.Convolution2D(filters=32, kernel_size=3, padding='same', activation='relu'))
@@ -47,7 +49,23 @@ if __name__ == "__main__":
     model.add(keras.layers.Dropout(0.25))
     model.add(keras.layers.Dense(10, activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    model.fit(X_train, Y_train, batch_size=32, epochs=15, verbose=1)
+    if data_augmentation:
+        datagen = keras.preprocessing.image.ImageDataGenerator(
+            featurewise_center=False,  # set input mean to 0 over the dataset
+            samplewise_center=False,  # set each sample mean to 0
+            featurewise_std_normalization=False,  # divide inputs by std of the dataset
+            samplewise_std_normalization=False,  # divide each input by its std
+            zca_whitening=False,  # apply ZCA whitening
+            rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+            width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+            height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+            horizontal_flip=True,  # randomly flip images
+            vertical_flip=False)  # randomly flip images
+        datagen.fit(X_train)
+        model.fit_generator(datagen.flow(X_train, Y_train, batch_size=batch_size), epochs=epochs, validation_data=(X_test, Y_test))
+    else:
+        model.fit(X_train, Y_train, batch_size=batch_size, epochs=epochs, verbose=2, validation_data=(X_test, Y_test))
     score = model.evaluate(X_test, Y_test)
     print(score)
     print(model.metrics_names)
+    print("End:%s" % (time.time() - start))
