9223839e1e7eece111dae205246ec1aede103a6d
Fillan Grady <fillangrady@gmail.com>
2018-04-13 08:20:04 -0500

    Added support for saving model

--- Passengers.py
+++ Passengers.py
@@ -1,5 +1,7 @@
 import keras
 import pandas
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import argparse
@@ -21,21 +23,48 @@ def create_dataset(dataset, look_back=1):
     return X, Y
 
 
+def save_model(model, file_name='Passengers'):
+    json_file_name = file_name + ".json"
+    h5_file_name = file_name + ".h5"
+    model_json = model.to_json()
+    with open(json_file_name, 'w') as json_file:
+        json_file.write(model_json)
+    model.save_weights(h5_file_name)
+
+
+def load_model(file_name='Passengers'):
+    json_file_name = file_name + ".json"
+    h5_file_name = file_name + ".h5"
+    loaded_model = None
+    with open(json_file_name, 'r') as json_file:
+        loaded_model = keras.models.model_from_json(json_file.read())
+    loaded_model.load_weights(h5_file_name)
+    return loaded_model
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-epochs', '-e', help="Number of epochs of training", type=int, default=100)
     parser.add_argument('-look_back', '-l', help="Number of periods to look back on", type=int, default=5)
+    parser.add_argument('-save', '-s', help="Save location", type=str, default=None)
+    parser.add_argument('-load', '-o', help="Load location", type=str, default=None)
     args = parser.parse_args()
     train, test = load_file()
     trainX, trainY = create_dataset(dataset=train, look_back=args.look_back)
     testX, testY = create_dataset(dataset=test, look_back=args.look_back)
-    model = keras.Sequential()
-    model.add(keras.layers.LSTM(units=4, input_shape=(1, args.look_back)))
-    model.add(keras.layers.Dense(units=1))
-    model.compile(loss='mean_squared_error', optimizer='adam')
-    model.fit(trainX, trainY, batch_size=1, epochs=args.epochs, verbose=2)
+    model = None
+    if args.load is None:
+        model = keras.Sequential()
+        model.add(keras.layers.LSTM(units=4, input_shape=(1, args.look_back)))
+        model.add(keras.layers.Dense(units=1))
+        model.compile(loss='mean_squared_error', optimizer='adam')
+        model.fit(trainX, trainY, batch_size=1, epochs=args.epochs, verbose=2)
+    else:
+        model = load_model(args.load)
     testPredict = model.predict(testX)
     plt.plot(trainY)
     plt.plot(np.array(range(train.size, train.size + test.size - args.look_back)), testPredict)
     plt.plot(np.array(range(train.size, train.size + test.size - args.look_back)), testY)
     plt.savefig("Passengers.png")
+    if args.save is not None:
+        save_model(model, file_name=args.save)
